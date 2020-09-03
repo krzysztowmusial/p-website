8890ee2cf928ac71c24f0bcae123b20a73aa4e9f
Krzysztof Musiał <mail@krzysztofmusial.com>
2020-09-03 18:35:59 +0200

    home component

--- src/app/website/components/home/home.component.ts
+++ src/app/website/components/home/home.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    home = null;
+    projects = null;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+        
+        this.home = {
+            about: {
+                name: 'Krzysztof Musiał',
+                title: 'Student / Web Developer',
+                desc: "Problem-solving always gave me a lot of satisfaction. That's why when I started learning to program, I knew that this is what I want to do in my life. I am very passionate about making my projects not only work well but also to be easy to read and improve in the future. Coding is something you have to learn constantly and I want my work to meet the standards of modern programming.",
+                data: {
+                    name: 'Krzysztof Musiał',
+                    location: 'Poznań, Poland',
+                    contact: '#'
+                },
+                links: {
+                    linkedin: '#',
+                    github: '#',
+                    dribbble: '#',
+                }
+            },
+            skills: {
+                frontend: ['html', 'css', 'js'],
+                backend: ['html', 'css', 'js'],
+                design: ['html', 'css', 'js'],
+                other: ['html', 'css', 'js'],
+            }
+        }
+
+        this.projects = ['1', '2', '3']
+
+    }
 
 }
