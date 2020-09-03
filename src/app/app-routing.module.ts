c7f381360479829d4bfdf49b82b6606ee2269d0c
Krzysztof Musiał <mail@krzysztofmusial.com>
2020-09-03 02:31:43 +0200

    website components added and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { WebsiteComponent } from './website/website.component';
 import { ResumeComponent } from './resume/resume.component';
+import { HomeComponent } from './website/components/home/home.component';
+import { ProjectsComponent } from './website/components/projects/projects.component';
+import { ContactComponent } from './website/components/contact/contact.component';
 
 
 const routes: Routes = [
-    {path: '', component: WebsiteComponent},
+    {path: '', component: WebsiteComponent, 
+        children: [
+            {path: '', component: HomeComponent},
+            {path: 'projects', component: ProjectsComponent},
+            {path: 'contact', component: ContactComponent},
+        ]},
     {path: 'resume', component: ResumeComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WebsiteComponent } from './website/website.component';
 import { ResumeComponent } from './resume/resume.component';
+import { MenuComponent } from './website/shared/components/menu/menu.component';
+import { NavComponent } from './website/shared/components/nav/nav.component';
+import { HomeComponent } from './website/components/home/home.component';
+import { ProjectsComponent } from './website/components/projects/projects.component';
+import { ContactComponent } from './website/components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WebsiteComponent,
     ResumeComponent,
+    MenuComponent,
+    NavComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
